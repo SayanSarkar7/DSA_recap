59ee720baec57fc3e3870d4816ccbc28e6eab326
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-06-08 22:17:01 +0530

    update reverse word in a geven string

---
+++ String/Reverse_words_in_a_given_string_/_Palindrome_Check.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words=s.trim().split(" ");
+        StringBuilder sb=new StringBuilder();
+
+        for(int i=words.length-1;i>=0;i--){
+            if(words[i]!=""){
+                sb.append(words[i]);
+                sb.append(" ");
+            }
+        }
+        return sb.substring(0,sb.length()-1).toString();// substring to remove last space
+
+    }
+}
+
+// Time Complexity, O(n), cause sb.append() takes O(n), if I have done adding in a string by += then the T.C becomes O(n)2
